6062840b1d62357b5a38879f3065cd5786fd41f3
Albina Salkayeva <salkaeva.a@gmail.com>
2024-06-16 22:29:47 +0500

    Add delay before the computer's move

--- src/BeginnerComputerOpponent.java
+++ src/BeginnerComputerOpponent.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import javax.swing.Timer;
 
 public class BeginnerComputerOpponent implements ComputerOpponent {
     private final Level level = Level.BEGINNER;
@@ -12,25 +13,29 @@ public class BeginnerComputerOpponent implements ComputerOpponent {
 
     @Override
     public void makeMove() {
-        List<Cell> emptyCellList = new ArrayList<>();
+        Timer timer = new Timer(2000, e -> {
+            List<Cell> emptyCellList = new ArrayList<>();
 
-        for (int i = 0; i < TicTacToe.BOARD_SIZE; i++) {
-            for (int j = 0; j < TicTacToe.BOARD_SIZE; j++) {
-                Cell cell = TicTacToe.getBoardCells()[i][j];
-                if (cell.getCellState() == CellState.EMPTY) {
-                    emptyCellList.add(cell);
+            for (int i = 0; i < TicTacToe.BOARD_SIZE; i++) {
+                for (int j = 0; j < TicTacToe.BOARD_SIZE; j++) {
+                    Cell cell = TicTacToe.getBoardCells()[i][j];
+                    if (cell.getCellState() == CellState.EMPTY) {
+                        emptyCellList.add(cell);
+                    }
                 }
             }
-        }
 
-        Random random = new Random();
-        int randomCellNumber = random.nextInt(emptyCellList.size());
-        Cell chosenCell = emptyCellList.get(randomCellNumber);
-        chosenCell.occupyCell();
+            Random random = new Random();
+            int randomCellNumber = random.nextInt(emptyCellList.size());
+            Cell chosenCell = emptyCellList.get(randomCellNumber);
+            chosenCell.occupyCell();
 
-        boolean isGameOver = TicTacToe.checkIfGameOver(chosenCell);
-        if (!isGameOver) {
-            TicTacToe.setIsPlayer1Turn(!TicTacToe.getIsPlayer1Turn());
-        }
+            boolean isGameOver = TicTacToe.checkIfGameOver(chosenCell);
+            if (!isGameOver) {
+                TicTacToe.setIsPlayer1Turn(!TicTacToe.getIsPlayer1Turn());
+            }
+        });
+        timer.setRepeats(false);
+        timer.start();
     }
 }
